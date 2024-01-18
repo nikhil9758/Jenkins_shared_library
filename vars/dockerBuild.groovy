c9d9b6dbb39144efef89c73902667ec67519dece
nikhil9758 <59034846+nikhil9758@users.noreply.github.com>
2024-01-18 09:08:22 +0000

    Update dockerBuild.groovy

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,8 +1,13 @@
 def call(String dockerHubUsername, String imageName) {
-    // Build the Docker image
-    sh "docker build --build-arg REACT_APP_RAPID_API_KEY=d245199e8emshd101669bdbafcf8p12d91ejsn53648793d8f6 -t ${imageName} ."
-     // Tag the Docker image
+    // Install buildx
+    sh 'docker buildx create --use'
+
+    // Build the Docker image with buildx
+    sh "docker buildx build --platform linux/amd64,linux/arm64 --build-arg REACT_APP_RAPID_API_KEY=d245199e8emshd101669bdbafcf8p12d91ejsn53648793d8f6 -t ${imageName} ."
+
+    // Tag the Docker image
     sh "docker tag ${imageName} ${dockerHubUsername}/${imageName}:latest"
+
     // Push the Docker image
     withDockerRegistry([url: 'https://index.docker.io/v1/', credentialsId: 'docker']) {
         sh "docker push ${dockerHubUsername}/${imageName}:latest"
